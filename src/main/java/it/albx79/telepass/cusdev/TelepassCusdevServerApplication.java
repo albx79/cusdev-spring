2c979995e59f785f43ad4d6b8d26110e5d4a36b2
Alberto Colombo <alberto.colombo@credimi.com>
2022-12-08 17:28:39 +0100

    Initial commit
    * skeleton project created with spring initializr
    * hand-written skeleton openapi spec
    
    The project compiles and runs (produces 501 on all endpoints as expected, since I haven't implemented anything yet.

---
+++ src/main/java/it/albx79/telepass/cusdev/TelepassCusdevServerApplication.java
@@ -0,0 +1,12 @@
+package it.albx79.telepass.cusdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class TelepassCusdevServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelepassCusdevServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/albx79/telepass/cusdev/TelepassCusdevServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.albx79.telepass.cusdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelepassCusdevServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
