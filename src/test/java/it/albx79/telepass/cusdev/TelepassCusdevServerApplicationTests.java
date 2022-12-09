ef7c22a0d4fdee37a62b83191a7a58105af2269f
Alberto Colombo <alberto.colombo@credimi.com>
2022-12-09 10:03:42 +0100

    Add spring integration tests
    Test that the Spring context is correctly setup and the controller calls our delegate. This is pretty much just a smoke test to check that the http layer is wired correctly.
    
    This way, the next tests I'm going to write will be either simple self-contained unit tests, or DB integration tests. Not touching Spring anymore.
    
    I took the opportunity to clean up the API and the code generator a bit.

--- src/test/java/it/albx79/telepass/cusdev/TelepassCusdevServerApplicationTests.java
+++ src/test/java/it/albx79/telepass/cusdev/TelepassCusdevServerApplicationTests.java
@@ -1,13 +1,55 @@
 package it.albx79.telepass.cusdev;
 
+import it.albx79.telepass.cusdev.api.CustomersApiDelegate;
+import it.albx79.telepass.cusdev.api.DevicesApiDelegate;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static javax.servlet.http.HttpServletResponse.SC_NO_CONTENT;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TelepassCusdevServerApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+	@MockBean
+	private CustomersApiDelegate customers;
+	@MockBean
+	private DevicesApiDelegate devices;
+
 	@Test
-	void contextLoads() {
+	void updateCustomerAddress_callsMethodOnDelegate() throws Exception {
+		when(customers.updateCustomerAddress(eq("foo"), anyString()))
+						.thenReturn(ResponseEntity.noContent().build());
+		mockMvc.perform(put("/customers/{customerId}/address", "foo")
+				.contentType(APPLICATION_JSON)
+				.content("""
+						"via montenapoleone 1, 20120 milano"
+						"""))
+				.andExpect(status().is(SC_NO_CONTENT));
 	}
 
+	@Test
+	void updateDeviceStatus_callsMethodOnDelegate() throws Exception {
+		when(devices.updateDeviceStatus(eq("foo"), anyString()))
+						.thenReturn(ResponseEntity.noContent().build());
+		mockMvc.perform(put("/devices/{deviceId}/status", "foo")
+				.contentType(APPLICATION_JSON)
+				.content("""
+						"INACTIVE"
+						"""))
+				.andExpect(status().is(SC_NO_CONTENT));
+	}
 }
